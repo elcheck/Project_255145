9b77a94f18108a450d15b4c492f9d8287709793a
EU\cekusjel <jelena.cekusina@tietoevry.com>
2022-02-23 18:53:16 +0200

    My first assignment correction (replaced int with float)

--- Day2Ex2.py
+++ Day2Ex2.py
@@ -3,8 +3,8 @@
 # Ask user to input 3 numbers - width, length, height
 # Find the volume of the room
 # PS Think about units and what is the most appropriate data type for this
-w=int(input("weight (cm)?:"))
-l=int(input("length (cm)?:"))
-h=int(input("height (cm)?:"))
-volume=w*l*h/100**3
-print(f"Room Volume {volume} 300m3")
+w=float(input("weight (cm)?:"))
+l=float(input("length (cm)?:"))
+h=float(input("height (cm)?:"))
+volume=round(w*l*h/100**3,2)
+print(f"Room Volume {volume} m3")
