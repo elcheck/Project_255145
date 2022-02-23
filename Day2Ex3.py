6bdcfd3f9c8df568e759ec0988553acce092c4e3
EU\cekusjel <jelena.cekusina@tietoevry.com>
2022-02-23 17:56:19 +0200

    My first assignment correction

--- Day2Ex3.py
+++ Day2Ex3.py
@@ -3,6 +3,6 @@
 # Write a program that asks user for temperature in Celsius and prints out this same temperature in Farenheit
 # formula is: farenheit = 32+celsius*(9/5)
 # PS Remember about data type conversion, also consider precision
-c=int(input("Celsius:"))
-f=32+c*(9/5)
+c=float(input("Celsius:"))
+f=round(32+c*(9/5),2)
 print(f"Farenheit: {f}")
