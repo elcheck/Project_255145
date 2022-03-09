d4e93c80d1d41b68c0e296478d1151a1f5987eb7
EU\cekusjel <jelena.cekusina@tietoevry.com>
2022-03-09 17:55:02 +0200

    day 6

--- day6_3.py
+++ day6_3.py
@@ -3,4 +3,4 @@ words=text.split()
 print(words)
 reversed_words=[word[::-1] for word in words]
 text_reversed=" ".join([word for word in reversed_words])
-print(text_reversed)
+print(text_reversed.capitalize())
