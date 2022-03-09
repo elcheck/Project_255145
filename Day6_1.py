8f55f6e87bb625158315e4a7766ef9c1584c6af1
EU\cekusjel <jelena.cekusina@tietoevry.com>
2022-03-09 17:41:13 +0200

    day 6

--- Day6_1.py
+++ Day6_1.py
@@ -2,12 +2,11 @@ numbers=[]
 while True:
     number=input("Enter number:(q to exit)")
     if number=='q':
-        if i<6:
+        if len(numbers)<6:
             print("Please more..")
             continue
         break
     else:
         numbers.append(float(number))
-        i+=1
-        sum+=float(number)
-print(f"{numbers[:3]}...{numbers[-3:]} average:{sum/i}")
+
+print(f"{numbers[:3]}...{numbers[-3:]} average:{sum(numbers)/len(numbers)}")
