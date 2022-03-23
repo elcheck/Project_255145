9880208fe0720437a489cc8ddad92711194c1805
EU\cekusjel <jelena.cekusina@tietoevry.com>
2022-03-23 17:25:09 +0200

    day 9

---
+++ Day9_2a.py
@@ -0,0 +1,4 @@
+def get_common_elements(seq1, seq2, seq3):
+        return tuple(set(seq1).intersection(set(set(seq2).intersection(set(seq3)))))
+
+print (get_common_elements("abcd", ['a', 'b','d'], ('b', 'c','d')))
