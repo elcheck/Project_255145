3b52c952eb75312c6f637db72791a1c7a08ea27c
EU\cekusjel <jelena.cekusina@tietoevry.com>
2022-03-23 17:25:53 +0200

    day 9

--- Day9.3.py
+++ Day9.3.py
@@ -1,5 +1,6 @@
 def is_pangram(text, alphabet='abcdefghijklmnopqrstuvwxyz'):
     return(len(tuple(set(alphabet).difference(set(text.lower()))))==0)
+
 text1="Not a pangram"
 print(f"text: {text1} -> {is_pangram(text1)}")
 text2="The five boxing wizards jump quickly"
