0053ebb87b182be033c368209382db462b81b854
EU\cekusjel <jelena.cekusina@tietoevry.com>
2022-04-18 20:18:54 +0300

    FinalWork.py

--- FinalWork.py
+++ FinalWork.py
@@ -7,9 +7,9 @@ class PlayersDB:
         self.cur.execute('DROP TABLE players')
     def create_tables(self):
         self.cur.execute(
-            'CREATE TABLE IF NOT EXISTS players (id INTEGER PRIMARY KEY AUTOINCREMENT,username TEXT, score INTEGER, level INTEGER)')
+            'CREATE TABLE IF NOT EXISTS players (id INTEGER PRIMARY KEY AUTOINCREMENT,username TEXT, score INTEGER)')
     def insert_player(self,username):
-        self.cur.execute('INSERT INTO players (username,score ) VALUES (?, ?, ?)', (username, 0, 0))
+        self.cur.execute('INSERT INTO players (username,score ) VALUES (?, ?)', (username, 0))
         self.conn.commit()
     def update_result(self, id, score):
         self.cur.execute('UPDATE players SET score = (?) WHERE id = (?);', (score, id))
@@ -33,19 +33,28 @@ class GuessWord:
 
 if __name__ == '__main__':
     username=input('Enter user name: ')
+    if username=='':
+        print('See you later!')
+        exit()
     session=PlayersDB('chinook.db')
+#    session.drop_table()
     session.create_tables()
     player=session.find_player(username)
-    if player[0]==0:
+    id=player[0]
+    if id==0:
         session.insert_player(username)
         print(f"Hello {username}! Welcome to the game!")
-    else:
+        player = session.find_player(username)
         id=player[0]
-        score=player[1]
-        print (f"Hello {username}!")
-        print(f"Nice to see you again! your score is: {score}")
-# here should be a code of playing game
-    the_name = input("Enter the text: ").upper()
+    else:
+        print (f"Hello {username}! Nice to see you again!")
+    score = player[1]
+    # here should be a code of playing game
+    with open('sentences.txt', encoding="utf-8") as f:  # create a file object
+         lines = f.readlines()
+    the_name=lines[0].upper()
+    print (the_name)
+    #    the_name = input("Enter the text: ").upper()
     result = ""
     for word in the_name.split(" "):
         result += "*" * len(word) + " "
@@ -71,9 +80,9 @@ if __name__ == '__main__':
             print(f"The result is {result}")
     print("Congratulations!!! You win!")
     score+=1
+    print(f"Your score is {score}")
     session.update_result(id,score)
     session.close()
-#    session.insert_data('test')
 
 
 
